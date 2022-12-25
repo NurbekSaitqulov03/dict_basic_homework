0e5ce415fc8093b93a29955c21dc18cfe9912200
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2022-12-25 21:22:06 +0500

    first_item.py

--- first_item.py
+++ first_item.py
@@ -3,5 +3,9 @@ def first_item(data):
     Given a dictionary, Return first item value.
     """
     data = {'a': 1, 'b': 2}
-    return data.get('a', 'not')
+    x = 0
+    while data:
+        key, value = data.popitem()
+        x = value
+    return x
 print(first_item({'a': 1, 'b': 2}))
