3c96b0e28b04d7b22710b7d1c72f87c81f06773f
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2022-12-25 20:57:50 +0500

    create_dictionary.py

--- create_dictionary.py
+++ create_dictionary.py
@@ -7,4 +7,10 @@ def create_dictionary(key, value):
     Returns:
         dict: dictionary with keys and values
     """
-    return
+    i = 0
+    a = {}
+    while i < len(key) and i < len(value):
+        a[key[i]] = value[i]
+        i+=1
+    return a
+print(create_dictionary([1, 2, 3], ["one", "two", "three"]))
