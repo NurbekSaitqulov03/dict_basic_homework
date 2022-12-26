7142973ac5cfc3edd260a64b8a3d9e3fc592b109
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2022-12-26 09:54:30 +0500

    last_item.py

--- last_item.py
+++ last_item.py
@@ -3,5 +3,5 @@ def last_item(data):
     Given a dictionary, Return last item value.
     """
     data = {'a': 1, 'b': 2}
-    return data.get('b', 'not')
+    return data.popitem()
 print(last_item({'a': 1, 'b': 2}))
